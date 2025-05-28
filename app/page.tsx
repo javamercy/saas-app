9d45bdc1006f1cda4f296f29708a9883ead74dad
Emre Kurşun <kursunemre01@gmail.com>
2025-05-28 07:51:14 +0300

    feat: add CompanionCard and CompanionList components; update main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
 
 const Page = () => {
   return (
     <>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>Lets Get Started</Button>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="1"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="2"
+          name="Countsy the Number Wizard"
+          topic="Derivates & Integrals"
+          subject="math"
+          duration={30}
+          color="#efd0ff"
+        />
+        <CompanionCard
+          id="3"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <div className="home-section">
+        <CompanionList />
+      </div>
     </>
   );
 };
---
+++ components/CompanionList.tsx
@@ -0,0 +1,5 @@
+const CompanionList = () => {
+  return <div>CompanionList</div>;
+};
+
+export default CompanionList;
