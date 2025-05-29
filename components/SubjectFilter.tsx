61556e14a79a1e9542bad4a43a1fc344a5ad7328
Emre Kurşun <kursunemre01@gmail.com>
2025-05-29 13:02:12 +0300

    feat: enhance CompanionsLibrary with search and filter functionality; add SearchInput and SubjectFilter components

---
+++ components/SubjectFilter.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { subjects } from "@/constants";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+
+const SubjectFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("subject") || "";
+
+  const [subject, setSubject] = useState(query);
+
+  useEffect(() => {
+    let newUrl = "";
+    if (subject === "all") {
+      newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["subject"],
+      });
+    } else {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "subject",
+        value: subject,
+      });
+    }
+
+    router.push(newUrl, { scroll: false });
+  }, [router, searchParams, subject]);
+  return (
+    <Select value={subject} onValueChange={setSubject}>
+      <SelectTrigger>
+        <SelectValue placeholder="Select subject" />
+      </SelectTrigger>
+      <SelectContent>
+        {subjects.map((subject) => (
+          <SelectItem key={subject} value={subject}>
+            {subject}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SubjectFilter;
